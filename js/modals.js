b9ea3d064415f621f7a0508404ea1b85791b15c1
eben-k <ennim.consults@gmail.com>
2019-01-22 14:35:07 +0000

    [feat #163323478]create template for admin page

--- js/modals.js
+++ js/modals.js
@@ -28,3 +28,27 @@ const x = setInterval(() => {
     document.getElementById("day").innerHTML = "The 2019 General Election is over.";
   }
 }, 1000);
+
+//Modals
+const modalPanel = document.querySelector(".modal-panel");
+// const cart = document.querySelector(".cart");
+const deleteItem = document.querySelector(".delete");
+const trigger = document.querySelector(".trigger");
+const close = document.querySelector(".close");
+const modalPanelEdit = document.querySelector(".modal-panel-edit");
+const triggerEdit = document.querySelector(".trigger-edit");
+
+
+const toggleModal = () => {
+    modalPanel.classList.toggle("show-modal-panel");
+}
+// const addToCart= () => {
+//     cart.classList.toggle("show-modal-panel");
+// }
+const toggleModalDelete= () => {
+    deleteItem.classList.toggle("show-modal-panel");
+}
+ 
+const toggleModalEdit = () => {
+    modalPanelEdit.classList.toggle("show-modal-panel");
+}

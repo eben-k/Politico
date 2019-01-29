8bad20b450a1582516f20702701b4674ede63b0a
eben-k <ennim.consults@gmail.com>
2019-01-29 10:54:56 +0000

    [fix]implement conflict feedbacks

--- server/app.js
+++ server/app.js
@@ -9,11 +9,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/api/v1', route);
-// app.get('/', (req, res) => {
-//   res.send({
-//     message: 'Welcome to Politico!',
-//   });
-// });
+
 
 app.use('*', (req, res, next) => {
   res.status(404).json({

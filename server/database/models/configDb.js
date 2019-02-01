350f018c9879d92a655da3166182f348566c636c
eben-k <ennim.consults@gmail.com>
2019-02-01 09:33:39 +0000

    [fix] implement feedback on database setup

--- server/database/models/configDb.js
+++ server/database/models/configDb.js
@@ -6,7 +6,7 @@ dotenv.config();
 let ssl = false;
 let connectionString = process.env.DATABASE_URL;
 if (process.env.NODE_ENV === 'production') ssl = true;
-if (process.env.NODE_ENV === 'test') connectionString = process.env.DATABASE_URL;
+if (process.env.NODE_ENV === 'test') connectionString = process.env.DATABASE_URL_TEST;
 const pool = new Pool({
   connectionString,
   ssl,
--- server/database/models/dropTables.js
+++ server/database/models/dropTables.js
@@ -1,8 +1,8 @@
 import pool from './configDb';
 
-pool.query('DROP TABLE IF EXISTS Users');
-pool.query('DROP TABLE IF EXISTS Parties');
-pool.query('DROP TABLE IF EXISTS Offices');
-pool.query('DROP TABLE IF EXISTS Candidates');
-pool.query('DROP TABLE IF EXISTS Votes');
-pool.query('DROP TABLE IF EXISTS Petitions');
+pool.query('DROP TABLE IF EXISTS users');
+pool.query('DROP TABLE IF EXISTS parties');
+pool.query('DROP TABLE IF EXISTS offices');
+pool.query('DROP TABLE IF EXISTS candidates');
+pool.query('DROP TABLE IF EXISTS votes');
+pool.query('DROP TABLE IF EXISTS petitions');

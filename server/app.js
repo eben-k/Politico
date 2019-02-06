d37846f6a241ef686ddb2969eb80066d3989edfc
eben-k <ennim.consults@gmail.com>
2019-02-06 15:32:54 +0000

    [chore

--- server/app.js
+++ server/app.js
@@ -14,6 +14,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(expressValidator());
 app.use(cors());
+app.use(express.static('UI'));
 
 app.use('/api/v1', route);
 
